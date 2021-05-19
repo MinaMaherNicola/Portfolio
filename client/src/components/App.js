5f6b269a6a0d58dcea27c55b1b76acefdf0e62e4
Mina Maher <minamahernicola@outlook.com>
2021-05-19 06:25:11 +0200

    Some experiments with client-side styling

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <p className="header neon-text-effect">
+        Min
+        <span className="neon-text-effect s-2">a</span> Ni
+        <span className="neon-text-effect s-36">c</span>
+        ola | Soft<span className="neon-effect s-25">w</span>are Engi
+        <span className="neon-text-effect s-35">n</span>eer
+      </p>
+    </div>
+  );
+};
+
+export default App;
